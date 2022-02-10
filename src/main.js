4eb9e0077ad0c0c8184b183882af1db3553e2d41
Eli Davidson <eli@shadowlab.dev>
2022-02-10 14:24:08 -0700

    Add iteration-2 requirements as comments

--- src/main.js
+++ src/main.js
@@ -28,3 +28,25 @@ function displayRandomBook() {
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+/*Iteration-2 REQUIREMENTS
+[ ] When a user clicks the “Make Your Own Cover” button, we should see the form, and the homepage view should be hidden
+
+[ ] When the Form view is visible, the “Show New Random Cover” and “Save Cover” buttons should be hidden
+
+[ ] When the Form view is visible, the “Home” button should be visible
+
+[ ] When a user clicks the “View Saved Covers” button, we should see the saved covers section, and the homepage view should be hidden
+
+[ ] When the Saved Covers view is visible, the “Show New Random Cover” and “Save Cover” buttons should be hidden
+
+[ ] When the Saved Covers view is visible, the “Home” button should be visible
+
+[ ] In summary: Be able to switch between the three views (main poster, form, and saved posters) on the correct button clicks
+
+[ ] When a user clicks the “Home” button, we should only see the Home section
+
+[ ] When a user clicks the “Home” button, the home button should be hidden
+
+[ ] When a user clicks the “Home” button, the “Show New Random Cover” and “Save Cover” buttons should be visible again
+*/
