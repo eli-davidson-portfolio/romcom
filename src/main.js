289320d144035b7a6d40724ac7288952412bd605
Eli Davidson <eli@shadowlab.dev>
2022-02-10 15:48:03 -0700

    Refactor global variables to  wrap in objects

--- src/main.js
+++ src/main.js
@@ -19,26 +19,48 @@
 
 [ ] When a user clicks the “Home” button, the “Show New Random Cover” and “Save Cover” buttons should be visible again
 */
+/* // Deprocated Global variables - DOM elements, current cover
+// var bookImage = document.querySelector('.cover-image');
+// var bookTitle = document.querySelector('.cover-title');
+// var bookDescriptor1 = document.querySelector('.tagline-1');
+// var bookDescriptor2 = document.querySelector('.tagline-2');
 
-// Global variables - DOM elements, current cover
-var bookImage = document.querySelector('.cover-image');
-var bookTitle = document.querySelector('.cover-title');
-var bookDescriptor1 = document.querySelector('.tagline-1');
-var bookDescriptor2 = document.querySelector('.tagline-2');
+// // Global variables - DOM elements, views
+// var homeView = document.querySelector('.home-view');
+// var savedView = document.querySelector('.saved-view');
+// var formView = document.querySelector('.form-view');
 
-// Global variables - DOM elements, views
-var homeView = document.querySelector('.home-view');
-var savedView = document.querySelector('.saved-view');
-var formView = document.querySelector('.form-view');
+// // Global variables - DOM elements, buttons
+// var homeButton = document.querySelector('.home-button');
+// var randomButton = document.querySelector('.random-cover-button');
+// var saveCoverButton = document.querySelector('.save-cover-button');
+// var viewSavedButton = document.querySelector('.view-saved-button');
+// var makeNewButton = document.querySelector('.make-new-button');
+*/
 
-// Global variables - DOM elements, buttons
-var homeButton = document.querySelector('.home-button');
-var randomButton = document.querySelector('.random-cover-button');
-var saveCoverButton = document.querySelector('.save-cover-button');
-var viewSavedButton = document.querySelector('.view-saved-button');
-var makeNewButton = document.querySelector('.make-new-button');
+// Global variables - DOM elements
+var view = {
+  home: document.querySelector('.home-view'),
+  saved: document.querySelector('.saved-view'),
+  form: document.querySelector('.form-view')
+}
 
-//Global variables - Data Arrays
+var book = {
+  image: document.querySelector('.cover-image'),
+  title: document.querySelector('.cover-title'),
+  descriptor1: document.querySelector('.tagline-1'),
+  descriptor2: document.querySelector('.tagline-2')
+}
+
+var button = {
+  home: document.querySelector('.home-button'),
+  random: document.querySelector('.random-cover-button'),
+  save: document.querySelector('.save-cover-button'),
+  viewSaved: document.querySelector('.view-saved-button'),
+  makeNew: document.querySelector('.make-new-button')
+}
+
+//Global variables - data arrays
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -46,15 +68,15 @@ var currentCover;
 
 //Event listeners
 window.addEventListener('load', displayRandomBook)
-randomButton.addEventListener('click', displayRandomBook)
+button.random.addEventListener('click', displayRandomBook)
 
 //Functions
 function displayRandomBook() {
-  bookImage.src = covers[getRandomIndex(covers)];
-  bookTitle.innerText = titles[getRandomIndex(titles)];
-  bookDescriptor1.innerText = descriptors[getRandomIndex(descriptors)];
-  bookDescriptor2.innerText = descriptors[getRandomIndex(descriptors)];
-  currentCover = new Cover(bookImage.src, bookTitle.innerText, bookDescriptor1.innerText, bookDescriptor2.innerText);
+  book.image.src = covers[getRandomIndex(covers)];
+  book.title.innerText = titles[getRandomIndex(titles)];
+  book.descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
+  book.descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(book.image.src, book.title.innerText, book.descriptor1.innerText, book.descriptor2.innerText);
 }
 
 function getRandomIndex(array) {
