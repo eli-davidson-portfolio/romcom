4be0f5ebf66a074f10de06cc34cc184aea01e021
Eli Davidson <eli@shadowlab.dev>
2022-02-10 14:27:24 -0700

    Refactor comments

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-// Create variables targetting the relevant DOM elements here :point_down:
+// Global variables - DOM
 var bookImage = document.querySelector('.cover-image');
 var bookTitle = document.querySelector('.cover-title');
 var bookDescriptor1 = document.querySelector('.tagline-1');
 var bookDescriptor2 = document.querySelector('.tagline-2');
 var randomButton = document.querySelector('.random-cover-button')
 
-// We've provided a few variables below
+//Global variables = Data Arrays
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-// Add your event listeners here :point_down:
+//Event listeners
 window.addEventListener('load', displayRandomBook)
 randomButton.addEventListener('click', displayRandomBook)
 
-// Create your event handlers and other functions here :point_down:
+//Functions
 function displayRandomBook() {
   bookImage.src = covers[getRandomIndex(covers)];
   bookTitle.innerText = titles[getRandomIndex(titles)];
@@ -24,7 +24,6 @@ function displayRandomBook() {
   currentCover = new Cover(bookImage.src, bookTitle.innerText, bookDescriptor1.innerText, bookDescriptor2.innerText);
 }
 
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
